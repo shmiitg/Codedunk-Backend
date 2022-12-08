4bd386deecd2eb0229484291f716dff8c7d760f4
Shubham Mondal <shubhamank002@gmail.com>
2022-12-08 20:53:58 +0530

    Add cors policy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
@@ -19,6 +20,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/", authRouter);
